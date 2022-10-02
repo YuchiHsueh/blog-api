3e26519497709a20fe8184bf0b00b792682ef4e0
Yuchi Hsueh <bhsueh94@gmail.com>
2022-10-02 11:49:30 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   Character.create(name: "Luke", movie: movies.first)
 Article.destroy_all
 
+Article.create!(name: "Asado", text: "Asado is the technique and the social event of having or attending a barbecue in various South American countries, especially Argentina, Chile, Paraguay and Uruguay where it is also a traditional event.")
+Article.create!(name: "Empanada", text: "An empanada is a type of baked or fried turnover consisting of pastry and filling, common in Spanish, other Southern European, Latin American, and Iberian-influenced cultures around the world.")
